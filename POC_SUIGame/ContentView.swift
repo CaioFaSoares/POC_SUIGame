094167d891b6bb35093589a86efde4cf236c0139
Caio Soares <caiofasoares@gmail.com>
2023-03-27 11:05:16 -0300

    Delegating stuff for SKS

--- POC_SUIGame/ContentView.swift
+++ POC_SUIGame/ContentView.swift
@@ -6,16 +6,50 @@
 //
 
 import SwiftUI
+import SpriteKit
 
+// A simple game scene with falling boxes
+class GameScene: SKScene, ObservableObject {
+    
+    override func didMove(to view: SKView) {
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        let box = SKSpriteNode(color: .red, size: CGSize(width: 50, height: 50))
+        box.position = location
+        box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 50, height: 50))
+        addChild(box)
+    }
+    
+    func generateBoxFromButtonClick() {
+        let box = SKSpriteNode(color: .red, size: CGSize(width: 50, height: 50))
+        box.position = CGPoint(x: 100, y: 100)
+        box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 50, height: 50))
+        addChild(box)
+    }
+    
+}
+
+// A sample SwiftUI creating a GameScene and sizing it
+// at 300x400 points
 struct ContentView: View {
+    @ObservedObject var gameScene = GameScene(
+        size: CGSize(width: 300, height: 400)
+    )
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack{
+            SpriteView(scene: gameScene)
+                .frame(width: 300, height: 400)
+                .ignoresSafeArea()
+            Button("Spawn Box") {
+                gameScene.generateBoxFromButtonClick()
+            }
+            
         }
-        .padding()
     }
 }
 
