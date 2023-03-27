bd61c9615030b0525404ec1fc7d54224ef5a4763
Caio <caiofasoares@gmail.com>
2023-03-27 08:59:35 -0300

    Initial Commit

---
+++ POC_SUIGame/POC_SUIGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  POC_SUIGameApp.swift
+//  POC_SUIGame
+//
+//  Created by Caio Soares on 27/03/23.
+//
+
+import SwiftUI
+
+@main
+struct POC_SUIGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
